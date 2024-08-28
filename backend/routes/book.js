2bfc31978ccaa7d72f4f649558c40a0409ff6680
Marwa Derbel <marwaderbel21@gmail.com>
2024-08-28 15:39:57 +0200

    activation

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -5,11 +5,11 @@ const multer = require ('../images/multer-config');
 
 const bookControllers = require('../controllers/books');
 
-router.get('/:id',auth, bookControllers.getOneBook);
-router.post('/',auth, multer, bookControllers.createBook);
+router.get('/:id', bookControllers.getOneBook);
+router.post('/',multer, bookControllers.createBook);
 router.put('/:id', auth, multer, bookControllers.modifyBook);
 router.delete('/:id',auth, bookControllers.deleteBook);
-router.get('/', auth, bookControllers.getAllBooks); 
+router.get('/', bookControllers.getAllBooks); 
 
 module.exports = router;
 
